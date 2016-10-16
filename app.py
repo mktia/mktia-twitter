fbe60fff71b7edab513f87b3a273e4190d408ac7
mktia <endoplasmicreticulum2468741059@gmail.com>
2016-10-17 00:14:17 +0900

    add func kataomoi, kataomoware, and teiki.revise html

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import tweepy
 from flask import Flask, render_template, session, request
 
 app = Flask(__name__)
+
 app.secret_key = os.environ['secret_key']
+url = 'http://mktia-twitter.herokuapp.com'
 
 consumer_key = os.environ['consumer_key']
 consumer_secret = os.environ['consumer_secret']
@@ -23,7 +25,7 @@ def auth():
 	except Exception as e:
 		print(e)
 		print('Error! Failed to set session.')
-	return render_template('auth.html', redirect_url=redirect_url)
+	return render_template('auth.html', url=url, redirect_url=redirect_url)
 
 @app.route('/menu')
 def select():
@@ -32,7 +34,7 @@ def select():
 	except Exception as e:
 		print(e)
 		print('Error! Failed to set sesstion.(verifier)')
-	return render_template('menu.html')
+	return render_template('menu.html', url=url)
 
 @app.route('/ffcheck')
 def ffcheck():
@@ -56,7 +58,7 @@ def ffcheck():
 	fo = len(followers)
 	
 	owner_id = api.me().id
-	api.create_list(name='kataomoi', mode='private', description='Initialized.')
+	api.create_list(name='kataomoi', mode='private', description="You follow this list members, but they don't follow you.")
 	
 	not_friends = []
 	cnt = 0
@@ -68,7 +70,7 @@ def ffcheck():
 		else:
 			not_friends.append(friends[i])
 	for user in not_friends:
-		if cnt < 180:
+		if cnt < 150:
 			try:
 				api.add_list_member(user_id=user, owner_id=owner_id, slug='kataomoi')
 				cnt += 1
@@ -77,7 +79,109 @@ def ffcheck():
 		else:
 			break
 	
-	return render_template('finish.html', type=1)
+	return render_template('finish.html', url=url, type=1)
+
+@app.route('/notfollow')
+def notfollow():
+	token = session.get('request_token')
+	verifier = session.get('verifier')
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_url)
+	auth.request_token = token
+	auth.get_access_token(verifier)
+	access_token = auth.access_token
+	access_token_secret = auth.access_token_secret
+	auth.set_access_token(access_token, access_token_secret)
+	api = tweepy.API(auth)
+	
+	friends = []
+	for friend in tweepy.Cursor(api.friends_ids).items():
+		friends.append(friend)
+	followers = []
+	for follower in tweepy.Cursor(api.followers_ids).items():
+		followers.append(follower)
+	
+	owner_id = api.me().id
+	api.create_list(name='notfollow', mode='private', description="This list members follow you, but you don't follow him.")
+	
+	not_follow = []
+	cnt = 0
+	
+	for fo in followers:
+		for fr in friends:
+			if fo == fr:
+				break
+		else:
+			not_follow.append(fo)
+	for user in not_follow:
+		if cnt < 150:
+			try:
+				api.add_list_member(user_id=user, owner_id=owner_id, slug='notfollow')
+				cnt += 1
+			except Exception as e:
+				print(e)
+		else:
+			break
+	
+	return render_template('finish.html', url=url, type=2)
+"""
+@app.route('/teiki')
+def teikicheck():
+	token = session.get('request_token')
+	verifier = session.get('verifier')
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_url)
+	auth.request_token = token
+	auth.get_access_token(verifier)
+	access_token = auth.access_token
+	access_token_secret = auth.access_token_secret
+	auth.set_access_token(access_token, access_token_secret)
+	api = tweepy.API(auth)
+	
+	friends = []
+	for friend in tweepy.Cursor(api.friends_ids).items():
+		friends.append(friend)
+
+	owner_id = api.me().id
+	api.create_list(name='teiki', mode='private', description="This list members tweet a lot of 'teiki' tweets.")
+	
+	teiki = []
+	count = 0
+	for fr in friends:
+		user = api.get_user(fr).screen_name
+		print user
+		tl = api.user_timeline(fr)
+		#count += 1
+		#if count > 5:
+		#	break
+		for j in range(5):
+			text1 = tl[j].text
+			num1 = text1.find(u'定期')
+			if num1 == -1:
+				for k in range(1, 5):
+					text2 = tl[j + k].text
+					num2 = text2.find(u'定期')
+					if num2 >= 0:
+						break
+				else:
+					teiki.append(fr)
+			else:
+				break
+	n = 0
+	for user in teiki:
+		if cnt < 150:
+			try:
+				api.add_list_member(user_id=user, owner_id=owner_id, slug='teiki')
+				n += 1
+			except Exception as e:
+				print(e)
+		else:
+			break
+			
+	return render_template('finish.html', type=3)
+"""
+	
+@app.route('/ver')
+def show_ver():
+	return render_template('ver.html', url=url)
 
 if __name__ == "__main__":
 	app.run()
